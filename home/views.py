5c634b41496e815ea5144db2efee254506e0c0cf
r0bertwell <r0bertwell@hotmail.com>
2022-05-19 01:01:42 -0300

    Modificado para deixar tudo na pasta template e static

--- home/views.py
+++ home/views.py
@@ -1,8 +1,8 @@
 from django.views.generic import TemplateView
 from mysite import urls
 class HomePageView(TemplateView):
-    template_name = 'home.html'
+    template_name = 'home/home.html'
 
 
 class ProjetosPageView(TemplateView):
-    template_name = 'projetos.html'
+    template_name = 'home/projetos.html'
--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -152,6 +151,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Default primary key field type
